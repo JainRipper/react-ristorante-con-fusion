0f809f1d2105322281ba31aecea385f0a872f690
Jainkarn P <jainripper@gmail.com>
2019-04-26 11:31:48 +1000

    initial commit

---
+++ build/precache-manifest.cdbdbfa8b74ed39831d585845d40724d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "cc224edb482c816acc79",
+    "url": "/static/css/main.eefc4e49.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "74015a3b5393e35289ec",
+    "url": "/static/js/2.5c546e68.chunk.js"
+  },
+  {
+    "revision": "cc224edb482c816acc79",
+    "url": "/static/js/main.ccfdf5fe.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "74015a3b5393e35289ec",
+    "url": "/static/css/2.8970dc16.chunk.css"
+  },
+  {
+    "revision": "ba00f689225d73a2e20f8220a963c5f2",
+    "url": "/index.html"
+  }
+];
