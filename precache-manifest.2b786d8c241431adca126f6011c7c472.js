ae9b70d72a59a7dafd9b02a7cb5cfa6e572d94bb
Jainkarn P <jainripper@gmail.com>
2019-04-29 18:56:57 +1000

    Updates

---
+++ precache-manifest.2b786d8c241431adca126f6011c7c472.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-confusion-beta/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "a26bc3860903e6b89ac5",
+    "url": "/react-confusion-beta/static/css/main.32d547d2.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-confusion-beta/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b4da40ced69749541224",
+    "url": "/react-confusion-beta/static/js/1.b4da40ce.chunk.js"
+  },
+  {
+    "revision": "e48c46d95e8e3ebe3ca6",
+    "url": "/react-confusion-beta/static/js/runtime~main.e48c46d9.js"
+  },
+  {
+    "revision": "a26bc3860903e6b89ac5",
+    "url": "/react-confusion-beta/static/js/main.a26bc386.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-confusion-beta/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-confusion-beta/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-confusion-beta/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b4da40ced69749541224",
+    "url": "/react-confusion-beta/static/css/1.e9c4cbe6.chunk.css"
+  },
+  {
+    "revision": "a6a72c5342378b65e02945cf2cc0cc2e",
+    "url": "/react-confusion-beta/index.html"
+  }
+];
